245066024efb910bf79e029ff5f834ec3bd40770
Glitch (glitch-hello-react) <none>
2022-10-25 15:08:38 +0200

    ok

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 /* ADD IMPORTS FROM TODO ON THE NEXT LINE */
-
+import { useWiggle } from "../hooks/wiggle";
+import { animated } from "react-spring";
 
 /**
 * The About function defines the component that makes up the About page
@@ -9,15 +10,15 @@ import * as React from "react";
 
 export default function About() {
   /* DECLARE STYLE AND TRIGGER FOR WIGGLE EFFECT FROM TODO ON NEXT LINE */
-  
+  const [style, trigger] = useWiggle({ x: 50, rotation: 1, scale: 1.2 });
   return (
     <div className="page">
       {/* REPLACE H1 ELEMENT BELOW WITH CODE FROM TODO */}
-      <h1 className="title">
-        About this site
-      </h1>
+      <animated.h1 className="title" style={style}>
+       About this site
+      </animated.h1>
       {/* REPLACE OPENING P TAG BELOW WITH CODE FROM TODO */}
-      <p>
+      <p onMouseEnter={trigger}>
         Welcome to the Glitch React starter, where you can instantly create a
         React site that's fully customizable.
       </p>
